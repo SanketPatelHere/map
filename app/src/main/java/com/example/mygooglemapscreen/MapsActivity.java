f7d54f5de2fd31a1b236f515b63add71b69c6673
ekta <ekta.elluminati@gmail.com>
2019-12-09 11:11:40 +0530

    Location display

---
+++ app/src/main/java/com/example/mygooglemapscreen/MapsActivity.java
@@ -0,0 +1,77 @@
+package com.example.mygooglemapscreen;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public GoogleMap mMap;
+    public TextView tvresult;
+    public GoogleMap.OnCameraIdleListener onCameraIdleListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        tvresult = (TextView)findViewById(R.id.tvresult);
+
+        configureCameraIdle();
+    }
+
+    public void configureCameraIdle()
+    {
+        onCameraIdleListener = new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                LatLng latLng = mMap.getCameraPosition().target;
+                Geocoder geocoder = new Geocoder(MapsActivity.this);
+                try
+                {
+                    List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                    if(addressList!=null && addressList.size()>0)
+                    {
+                        String locality = addressList.get(0).getAddressLine(0);
+                        String country = addressList.get(0).getCountryName();
+                        if(!locality.isEmpty() && !country.isEmpty())
+                        {
+                            tvresult.setText("Location = "+locality+" , "+country);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.i("My Exception",e+"");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnCameraIdleListener(onCameraIdleListener);
+        // Add a marker in Sydney and move the camera
+        /*LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+
+    }
+}
